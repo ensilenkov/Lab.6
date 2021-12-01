8713743acc71c57cfd923d27c1fb28cae3d13e51
Евгений Силенков <ensilenkov@gmail.com>
2021-12-01 22:58:22 +0700

    Добавьте файлы проекта.

---
+++ Lab.6.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab._6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = Console.ReadLine();
+            string s2 = s1.Replace(" ", "");
+            string s3 = "";
+
+            for (int i = s2.Length - 1; i >= 0; i--)
+            {
+                s3 += s2[i];
+            }
+            if (s2 == s3)
+                Console.WriteLine("Палиндром!");
+            else
+                Console.WriteLine("Не палиндром!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab.6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            int max = 0;
+            string s1 = stringArray[0];
+
+            foreach (string s in stringArray)
+            {
+                if ( s.Length >= max)   
+                { 
+                    max = s.Length;
+                    s1 = s;
+                    
+                }
+                else
+                {
+                    max = max;
+                }
+            }
+
+            Console.Write(s1);
+            Console.ReadKey();
+        }
+    }
+}
